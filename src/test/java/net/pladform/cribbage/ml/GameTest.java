e6bb1a94fd4f23b69c18dfde62a53799e8ac04b2
Dan Barrese <danielbarrese@gmail.com>
2016-06-23 08:15:14 -0600

    timers

--- src/test/java/net/pladform/cribbage/ml/GameTest.java
+++ src/test/java/net/pladform/cribbage/ml/GameTest.java
@@ -1,15 +1,19 @@
 package net.pladform.cribbage.ml;
 
+import net.pladform.cribbage.engine.Card;
 import net.pladform.cribbage.engine.Deck;
 import net.pladform.cribbage.engine.Game;
 import net.pladform.cribbage.engine.Player;
 import net.pladform.cribbage.engine.Scoreboard;
+import net.pladform.cribbage.engine.Suit;
 import net.pladform.perf.Timer;
 import org.junit.Test;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -23,7 +27,6 @@ public class GameTest {
 
     @Test
     public void testPlay() {
-        Timer.start("game setup");
         Scoreboard scoreboard = new Scoreboard();
         Deck deck = new Deck();
         Game game = new Game(scoreboard, deck);
@@ -31,21 +34,22 @@ public class GameTest {
         Player ken = new Player("Ken", game);
         scoreboard.setPlayers(new Player[]{dan, ken});
         scoreboard.setDealer(dan);
-        Timer.stop("game setup");
         Timer.start("game play");
         Map<String, List<Integer>> gameStats = game.play();
+        Timer.stop("game play");
         gameStats.forEach((state, finalPoints) -> stats.merge(state, finalPoints, (integers, integers2) -> {
             integers.addAll(integers2);
             return integers;
         }));
-        Timer.stop("game play");
+        System.out.println(gameStats);
     }
 
     @Test
     public void testGames() throws Exception {
-        Timer.start("test run time");
-        int gamesPerThread = 10_000;
-        int threads = Runtime.getRuntime().availableProcessors();
+//        Timer.disable();
+        int gamesPerThread = 10;
+//        int threads = Runtime.getRuntime().availableProcessors();
+        int threads = 1;
         System.out.println(String.format("threads: %d", threads));
         ExecutorService es = Executors.newFixedThreadPool(threads);
         for (int i = 0; i < threads; i++) {
@@ -55,8 +59,7 @@ public class GameTest {
                         Timer.start("entire game");
                         testPlay();
                         Timer.stop("entire game");
-                    }
-                    catch (Exception e) {
+                    } catch (Exception e) {
                         e.printStackTrace();
                     }
                 }
@@ -64,11 +67,37 @@ public class GameTest {
         }
         es.shutdown();
         es.awaitTermination(1L, TimeUnit.DAYS);
-        Timer.stop("test run time");
         System.out.println(Timer.getStats());
         RoundStatsBuckets roundStats = new RoundStatsBuckets();
         stats.forEach((state, pointSpread) -> pointSpread.forEach(points -> roundStats.add(state, points)));
 //        System.out.println(roundStats);
     }
+
+//    @Test
+//    public void testGameFromStartingPoint() throws Exception {
+//        Scoreboard scoreboard = new Scoreboard();
+//        Deck deck = new Deck();
+//        Game game = new Game(scoreboard, deck);
+//        Player dan = new Player("Dan", game);
+//        Player ken = new Player("Ken", game);
+//        scoreboard.setPlayers(new Player[]{dan, ken});
+//        scoreboard.setDealer(dan);
+//
+//        Set<Card> cards = new HashSet<>();
+//        cards.add(deck.select(Card.Type.ACE, Suit.CLUBS));
+//        cards.add(deck.select(Card.Type._2, Suit.SPADES));
+//        cards.add(deck.select(Card.Type._3, Suit.DIAMONS));
+//        cards.add(deck.select(Card.Type._4, Suit.HEARTS));
+//        cards.add(deck.select(Card.Type._5, Suit.CLUBS));
+//        cards.add(deck.select(Card.Type._6, Suit.SPADES));
+//
+//        Timer.start("game play");
+//        Map<String, List<Integer>> gameStats = game.playFromStartingPoint(dan, cards);
+//        Timer.stop("game play");
+//        gameStats.forEach((state, finalPoints) -> stats.merge(state, finalPoints, (integers, integers2) -> {
+//            integers.addAll(integers2);
+//            return integers;
+//        }));
+//    }
 
 }
